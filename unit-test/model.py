4461bceb798894183ce493e97aa000b007109b33
Xayah <ycxiang14sep@gmail.com>
2024-10-24 11:12:12 +0900

    update rewrite[1]

--- unit-test/model.py
+++ unit-test/model.py
@@ -1,24 +1,39 @@
-from nerfstudio.models.base_model import Model, ModelConfig
-from nerfstudio.cameras.cameras import Cameras
-from nerfstudio.cameras.rays import RayBundle
+import nerfstudio.models.base_model
+import nerfstudio.cameras.cameras
+import nerfstudio.cameras.rays
+import nerfstudio.data.scene_box
+import nerfstudio.field_components.encodings
 
 import torch
 import dataclasses
 import typing
 
 
-class PINFNeRFModelConfig(ModelConfig):
+class PINFNeRFModelConfig(nerfstudio.models.base_model.ModelConfig):
     _target: typing.Type = dataclasses.field(default_factory=lambda: PINFNeRFModel)
 
 
-class PINFNeRFModel(Model):
-    def __init__(self):
-        super().__init__()
+class PINFNeRFModel(nerfstudio.models.base_model.Model):
+    def __init__(self, config: PINFNeRFModelConfig, scene_box: nerfstudio.data.scene_box.SceneBox, num_train_data: int):
+        self.field_coarse = None
+        self.field_fine = None
+
+        super().__init__(config=config, scene_box=scene_box, num_train_data=num_train_data)
+
+    def populate_modules(self):
+        super().populate_modules()
+
+        position_encoding = nerfstudio.field_components.encodings.NeRFEncoding(
+            in_dim=3, num_frequencies=10, min_freq_exp=0.0, max_freq_exp=8.0, include_input=True
+        )
+        direction_encoding = nerfstudio.field_components.encodings.NeRFEncoding(
+            in_dim=3, num_frequencies=4, min_freq_exp=0.0, max_freq_exp=4.0, include_input=True
+        )
 
     def get_param_groups(self) -> typing.Dict[str, typing.List[torch.nn.Parameter]]:
         pass
 
-    def get_outputs(self, ray_bundle: typing.Union[RayBundle, Cameras]) -> typing.Dict[str, typing.Union[torch.Tensor, typing.List]]:
+    def get_outputs(self, ray_bundle: typing.Union[nerfstudio.cameras.rays.RayBundle, nerfstudio.cameras.cameras.Cameras]) -> typing.Dict[str, typing.Union[torch.Tensor, typing.List]]:
         pass
 
     def get_loss_dict(self, outputs, batch, metrics_dict=None) -> typing.Dict[str, torch.Tensor]:
